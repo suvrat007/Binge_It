eefc4be0a2c6fc0776bacaa4ef74994895b2ae8b
Suvrat <suvratmittal007@gmail.com>
2024-11-27 22:01:53 +0530

    final final commit pt 2

--- src/utils/openai.js
+++ src/utils/openai.js
@@ -1,8 +1,8 @@
-import OpenAI from 'openai';
-
-const openai = new OpenAI({
-    apiKey: process.env.REACT_APP_OPENAI_KEY,
-    dangerouslyAllowBrowser: true// This is the default and can be omitted
-});
-
-export default openai;
+// import OpenAI from 'openai';
+//
+// const openai = new OpenAI({
+//     apiKey: process.env.REACT_APP_OPENAI_KEY,
+//     dangerouslyAllowBrowser: true// This is the default and can be omitted
+// });
+//
+// export default openai;
