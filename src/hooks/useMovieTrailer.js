2efa9a71a3f3e243cbb8036bbd69541f570509c6
Suvrat <suvratmittal007@gmail.com>
2024-11-27 12:48:40 +0530

    made api work

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,6 +1,6 @@
 import {useDispatch, useSelector} from "react-redux";
 import {addTrailerVideo} from "../utils/movieSlice";
-import {useEffect, useRef} from "react";
+import {useEffect} from "react";
 import {API_OPTIONS} from "../utils/constatants";
 
 const useMovieTrailer = (movieId) => {
@@ -10,10 +10,14 @@ const useMovieTrailer = (movieId) => {
     const getMovieVideos = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/' +movieId+'/videos?language=en-US', API_OPTIONS);
         const json = await data.json();
-        // console.log("THIS JSON" , json);
-        const filterData = json.results.filter((video) => video.type === "Trailer");
-        // what if trailer dont have video
-        const trailer = filterData.length? filterData[0] : json.results[0];
+
+        console.log("THIS JSON" , json);
+        const filterData = json.results.filter((video) => video?.type === "Trailer");
+
+
+        // what if trailer don't have video
+        const trailer = (filterData.length)? filterData[0] : json.results[0];
+
         // console.log("trailer = " ,trailer);
         // setTrailerId(trailer.key);
         dispatch(addTrailerVideo(trailer));
--- src/utils/constatants.js
+++ src/utils/constatants.js
@@ -8,7 +8,7 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer '+ process.env.REACT_APP_API_TOKEN,
+        Authorization: 'Bearer '+ process.env.REACT_APP_TMDB_ACCESS_TOKEN ,
 
     }
 };
