85cb413af1dc0c92e3a38ad1cf30bd1ae16666fc
Suvrat <suvratmittal007@gmail.com>
2024-11-27 13:09:04 +0530

    sugesstion box changes in tailwind

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,12 +3,12 @@ import MovieCard from "./MovieCard";
 const MovieList = ({ title, movies }) => {
     console.log(movies);
     return (
-        <div className="px-4">
+        <div className="py-2 md:py-4  px-6 md:px-10">
             <h1 className="text-lg md:text-3xl py-2 text-white">{title}</h1>
             <div className="flex shadow-black shadow-xl overflow-x-scroll ">
                 <div className="flex">
                     {movies?.map((movie) => (
-                        <MovieCard key={movie.id} posterPath={movie.poster_path}/>))})
+                        <MovieCard key={movie.id} posterPath={movie.poster_path}/>))}
                     {/*<MovieCard posterPath={movies[0].poster_path} />*/}
                 </div>
             </div>
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -13,11 +13,10 @@ const VideoBackground = ({movieId}) =>{
         // ALWAYS use camel case in JSX
         <div className="w-screen">
             <iframe className="w-screen aspect-video"
-                    src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1"}
+                    src={"https://www.youtube.com/embed/" + trailerVideo?.key + "?&autoplay=1&mute=1&loop=1&controls=0&playlist="+trailerVideo?.key}
                     title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;"
                     referrerPolicy="strict-origin-when-cross-origin" >
-
             </iframe>
         </div>
     )
